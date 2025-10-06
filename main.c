04633688d62b290cc19c0e0e6ae4c81e043a204c
Rogelio Gudino <cananito@gmail.com>
2025-10-05 18:47:39 -0700

    Reformats.

--- main.c
+++ main.c
@@ -6,15 +6,21 @@ int main() {
   // TODO: Get input from args.
   char* input = "programming";
 
-  CFStringRef input_string = CFStringCreateWithCString(NULL, input, kCFStringEncodingUTF8);
+  CFStringRef input_string = CFStringCreateWithCString(NULL,
+                                                       input,
+                                                       kCFStringEncodingUTF8);
   CFRange input_range = CFRangeMake(0, CFStringGetLength(input_string));
 
-  CFStringRef result_string = DCSCopyTextDefinition(NULL, input_string, input_range);
+  CFStringRef result_string = DCSCopyTextDefinition(NULL,
+                                                    input_string,
+                                                    input_range);
 
   CFRelease(input_string);
 
-  char* result = calloc(CFStringGetLength(result_string) * 2, sizeof(char));
-  CFStringGetCString(result_string, result, CFStringGetLength(result_string) * 2, kCFStringEncodingUTF8);
+  // TODO: Do a more precise allocation size.
+  CFIndex buffer_size = CFStringGetLength(result_string) * 2;
+  char* result = calloc(buffer_size, sizeof(char));
+  CFStringGetCString(result_string, result, buffer_size, kCFStringEncodingUTF8);
 
   CFRelease(result_string);
 
